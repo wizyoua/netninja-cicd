a19795897283dd7a4bffebba0923feb3277cf8e2
Gianni Zamora <giannizcool@gmail.com>
2022-09-27 14:04:26 -0700

    Add Sentry error reportingto React application

--- src/index.js
+++ src/index.js
@@ -2,6 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import * as Sentry from "@sentry/react";
+import { BrowserTracing } from "@sentry/tracing";
+
+Sentry.init({
+  dsn: "https://e86e030fcd1048e9b5e6c57db902cc6e@o1429363.ingest.sentry.io/6780008",
+  integrations: [new BrowserTracing()],
+
+  // Set tracesSampleRate to 1.0 to capture 100%
+  // of transactions for performance monitoring.
+  // We recommend adjusting this value in production
+  tracesSampleRate: 1.0,
+});
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
